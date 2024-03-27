ed080fe81c3f65387dd09c53e9d1605a17734485
Arkinetic <danielraketic9@gmail.com>
2024-03-27 22:51:56 +0100

    Dynamic Coordinates /reformat

--- src/main/java/quixl/arkinetic/arkntmadditions/Upgrades/EngineBooster.java
+++ src/main/java/quixl/arkinetic/arkntmadditions/Upgrades/EngineBooster.java
@@ -1,35 +1,12 @@
 package quixl.arkinetic.arkntmadditions.Upgrades;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.Monitor;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.tardis.mod.controls.HandbrakeControl;
-import net.tardis.mod.controls.MonitorControl;
 import net.tardis.mod.controls.ThrottleControl;
-import net.tardis.mod.controls.XControl;
-import net.tardis.mod.misc.Console;
 import net.tardis.mod.misc.ITickable;
-import net.tardis.mod.misc.ObjectWrapper;
-import net.tardis.mod.misc.SpaceTimeCoord;
 import net.tardis.mod.subsystem.Subsystem;
 import net.tardis.mod.tileentities.ConsoleTile;
-import net.tardis.mod.tileentities.exteriors.ExteriorTile;
 import net.tardis.mod.upgrades.Upgrade;
 import net.tardis.mod.upgrades.UpgradeEntry;
-import net.tardis.mod.world.dimensions.VortexChunkGenerator;
-import quixl.arkinetic.arkntmadditions.ARKNTMAdditions;
 import quixl.arkinetic.arkntmadditions.RegSoundEvents;
 
 public class EngineBooster extends Upgrade implements ITickable {
